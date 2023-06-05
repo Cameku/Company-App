31f4d64b30a584aaf93d2b179460a3f987095df2
Cameku <cameku@gmail.com>
2023-06-05 13:21:32 +0100

    created new company and employees
    New company created
    New employee list created
    Delete method implemented

--- src/routes/CreateCompany.tsx
+++ src/routes/CreateCompany.tsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import { Button, Col, Container, Form, Row } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import { Employee } from '../types';
+
 
 const CreateCompany = () => {
 
-    const handleAddEmployee = () => {
+    const [employeeList, setEmployeeList] = useState<Employee[]>([]);
+    const [indexBeingEdited, setIndexBeingEdited] = useState(-1);
+    const [employeeName, setEmployeeName] = useState('');
+    const [employeeAge, setEmployeeAge] = useState(Number);
 
+
+    const addEmployee = () => {
+        if (employeeName.trim().length !== 0 && employeeAge > 0) {
+            setEmployeeList(employeeList => [...employeeList, { name: employeeName, age: employeeAge }]);
+            setEmployeeName('');
+            setEmployeeAge(0)
+        } else {
+            prompt('Input value cannot be empty')
+        }
+
+    }
+
+    const handleDeleteEmployee = (index: number) => {
+        setEmployeeList(newEmployeeList => { return newEmployeeList.filter((_, i) => i !== index) });
+    }
+
+    const editEmployee = (index: number) => {
+        setIndexBeingEdited(index);
+    }
+
+    const handleInput = (e: any) => {
+        const newName = employeeList.map((empName) => (
+            empName.name === employeeName ? { ...empName, [e.target.name]: e.target.value } : empName
+        ))
+        setEmployeeList(newName);
+    }
+
+    const updateEmployee = (name: string, age: number) => {
+        setEmployeeList(employeeList => [...employeeList, { name: employeeName, age: employeeAge }])
+        //setEmployeeName(name);
+        //setEmployeeAge(age);
     }
 
 
@@ -13,9 +49,9 @@ const CreateCompany = () => {
             <Container style={{ paddingTop: 50 }}>
                 <Row>
                     <Col>
-                        <h4>Create new Company</h4>
+                        <h4>New Company</h4>
                         <Form>
-                            <Form.Group style={{ width: 600 }} className="mb-3" controlId="formCompany">
+                            <Form.Group className="mb-3" controlId="formCompany">
                                 <Form.Label>Name</Form.Label>
                                 <Form.Control type="text" placeholder="Enter Company Name" />
                                 <Form.Label>Address</Form.Label>
@@ -23,25 +59,77 @@ const CreateCompany = () => {
                                 <Form.Label>Website</Form.Label>
                                 <Form.Control type="text" placeholder="Website" />
                             </Form.Group>
-                            <hr style={{ height: 1, backgroundColor: 'black' }} />
-                            <Form.Group style={{ width: 600 }} className="mb-3" controlId="formEmployee">
+                            {/* <hr style={{ height: 1, backgroundColor: 'black' }} /> */}
+                            <Form.Group className="mb-3" controlId="formEmployee">
                                 <Form.Label>Name </Form.Label>
                                 <Form.Control
                                     type="text"
-                                    placeholder="Employee Name" />
+                                    placeholder="Employee Name"
+                                    value={employeeName}
+                                    onChange={(e) => setEmployeeName(e.target.value)} />
                                 <Form.Label>Age </Form.Label>
-                                <Form.Control type="text" placeholder="Employee Age" />
-
+                                <Form.Control
+                                    type='number'
+                                    pattern='[0-9]*'
+                                    placeholder="Employee Age"
+                                    value={employeeAge}
+                                    onChange={(e) => setEmployeeAge(Number(e.target.value))} />
                             </Form.Group>
-                            <Button variant="primary" onClick={handleAddEmployee}> Add Employee </Button> { }
-                            <Button variant="danger" type="submit"> Delete Employee </Button> { }
+
+                            <Button variant="primary" onClick={addEmployee} > Add Employee </Button> { }
                             <Button variant="success" type="submit"> Add To Company </Button> { }
-                            <Button variant='info'>Back Home</Button>
+                            <Button variant='warning'>Back Home</Button>
                         </Form>
                     </Col>
                     <Col>
                         <h4>New Employee list</h4>
-                        <p>Name: {'Cathy'}  - Age: {20}</p>
+                        {
+                            employeeList?.length > 0 ? (
+                                employeeList.map((employee, index) => (
+                                    indexBeingEdited === index ?
+                                        <div key={index}>
+                                            <Row className='mb-2'>
+                                                <Col>
+                                                    <input
+                                                        className="mr-3"
+                                                        value={employee.name}
+                                                        type="text"
+                                                        onChange={handleInput}
+                                                    //onChange={(e) => setEmployeeName(employee.name)}
+                                                    />
+                                                    <input
+                                                        className="mr-3"
+                                                        value={employee.age}
+                                                        type="number"
+                                                        placeholder="Employee Age"
+                                                        onChange={(e) => setEmployeeAge(Number(e.target.value))}
+                                                    //onChange={(e) => handleChange(Number(employee.age, e))} 
+                                                    />
+                                                </Col>
+
+                                                <Button variant="danger" size='sm' className='mr-2'
+                                                    onClick={() => { handleDeleteEmployee(index) }}>Delete </Button>
+                                                <Button variant="secondary" size='sm' onClick={() => { updateEmployee(employee.name, employee.age) }}
+                                                >Update </Button>
+                                            </Row>
+                                        </div>
+                                        :
+                                        <div key={index}>
+                                            <Row className='mb-1'>
+                                                <Col>
+                                                    Name: <b className='mr-3'>{employee.name}</b>
+                                                    Age: <b className='mr-3'>{employee.age}</b> </Col>
+                                                <Button variant="danger" size='sm' className='mr-2'
+                                                    onClick={() => { handleDeleteEmployee(index) }} >Delete </Button>
+                                                <Button variant="secondary" size='sm' onClick={() => { editEmployee(index) }}
+                                                >Edit Employee </Button>
+                                            </Row>
+                                        </div>
+                                ))
+                            ) : (
+                                <div><h6>Please add an employee to the list</h6></div>
+                            )
+                        }
                     </Col>
                 </Row>
 
@@ -52,3 +140,5 @@ const CreateCompany = () => {
 }
 
 export default CreateCompany
+
+
